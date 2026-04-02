0a9a6ec3507e8e96003a9af4e6b5fc842b69cd16
yehormiaus <myaus.egor@gmail.com>
2026-04-02 18:29:26 +0100

    fix: use t3.micro (Free Tier eligible for newer AWS accounts)

--- infra/environments/dev-ephemeral/variables.tf
+++ infra/environments/dev-ephemeral/variables.tf
@@ -15,7 +15,7 @@ variable "aws_region" {
 }
 
 variable "instance_type" {
-  description = "EC2 instance type (t2.micro = Free Tier eligible)"
+  description = "EC2 instance type (t3.micro = Free Tier eligible)"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
